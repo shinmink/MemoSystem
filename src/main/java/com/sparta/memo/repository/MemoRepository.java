a62f89917ddaa03087b87ab33fe04f76eff28aed
shinmink <sinmk99@hansung.ac.kr>
2024-05-21 19:48:39 +0900

    Query Methods란 무엇일까

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -1,27 +1,10 @@
 package com.sparta.memo.repository;
 
-import com.sparta.memo.dto.MemoRequestDto;
-import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
-import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
-@Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
-
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
